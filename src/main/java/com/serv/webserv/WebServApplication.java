00bf3e380ab087f721f230cf40cf7ffc273a0b99
Andrey Karelin <kkarelinn@gmail.com>
2022-01-25 13:26:02 +0200

    Start lab3

---
+++ src/main/java/com/serv/webserv/WebServApplication.java
@@ -0,0 +1,13 @@
+package com.serv.webserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServApplication.class, args);
+    }
+
+}
